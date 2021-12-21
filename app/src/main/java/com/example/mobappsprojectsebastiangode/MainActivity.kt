a018fbe21f51429163b9ed04458141ebdeb7143b
SebastianGode <sebastian@gode-familie.de>
2021-12-21 13:10:01 +0100

    Added Fragment Code

--- app/src/main/java/com/example/mobappsprojectsebastiangode/MainActivity.kt
+++ app/src/main/java/com/example/mobappsprojectsebastiangode/MainActivity.kt
@@ -6,17 +6,19 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
@@ -27,15 +29,33 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
+            // Highlight the current selected Item
+            it.isChecked = true
+
+            // Switch Fragments when user clicks on Items in NavDrawer
             when(it.itemId) {
-                R.id.nav_home -> Toast.makeText(applicationContext, "Cliked Home", Toast.LENGTH_SHORT).show()
-                R.id.nav_import -> Toast.makeText(applicationContext, "Cliked Import", Toast.LENGTH_SHORT).show()
-                R.id.nav_export -> Toast.makeText(applicationContext, "Cliked Export", Toast.LENGTH_SHORT).show()
-                R.id.nav_share -> Toast.makeText(applicationContext, "Cliked Share", Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> replaceFragment(HomeFragment(), it.title.toString())
+                R.id.nav_import -> replaceFragment(ImportFragment(), it.title.toString())
+                R.id.nav_export -> replaceFragment(ExportFragment(), it.title.toString())
+                R.id.nav_share -> replaceFragment(ShareFragment(), it.title.toString())
             }
 
             true
         }
+    }
+
+    // Method for replacing fragments
+    private fun replaceFragment(fragment: Fragment, title : String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+        // Close Drawer when user clicks on it
+        drawerLayout.closeDrawers()
+
+        // Set the correct title of current fragment
+        setTitle(title)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
